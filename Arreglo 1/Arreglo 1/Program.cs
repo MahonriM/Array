f32316cee033cf35f2af25e484f1d3c15b7adb39
MahonriM <cripto3000z@gmail.com>
2019-04-16 19:11:22 -0500

    Add files via upload

---
+++ Arreglo 1/Arreglo 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arreglo_1
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {/*Hacer un programa que declare un arreglo de una dimensión con
+          valores predefinidos y los muestre.*/
+            int[] numeros= { 0,1,2,3,5,8,10,11,15,20};
+            foreach (int i in numeros)
+            {
+                Console.WriteLine("{0}", i);
+            }
+
+            //Pausa
+            Console.WriteLine("Presiona cualquier tecla para continuar");
+            Console.ReadKey();
+            
+        }
+        
+            
+        
+    }
+}
